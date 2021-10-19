d2f8da4a0497c0e69226510927c7389137c48c71
Christian E <christianelena92@gmail.com>
2021-10-19 17:26:30 -0500

    add signout button and dropdown lable hover effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -84,6 +84,12 @@ const Header = (props) => {
               <span>WATCHLIST</span>
             </a>
           </NavMenu>
+          <SignOut>
+            {/* <UserImg src={userPhoto} alt={username} /> */}
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </SignOut>
           <UserImg src={userPhoto} alt={username} />
         </>
       )}
@@ -199,5 +205,41 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rbg(0 0 0 / 50%) 0px 0px 18px 0;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &: hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 export default Header;
